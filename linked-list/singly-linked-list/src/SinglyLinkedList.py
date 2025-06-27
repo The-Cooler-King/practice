8c2be39595d812d21c4187d25940ab064ea42054
The-Cooler-King <vham0927@gmail.com>
2025-06-27 16:11:49 -0700

    LLPP-0007: copied node class from indexed-linked-list; started SinglyLinkedList methods

---
+++ linked-list/singly-linked-list/src/SinglyLinkedList.py
@@ -0,0 +1,30 @@
+from Node import Node
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self):
+        return
+
+    def prepend(self):
+        return
+
+    def pop(self):
+        return
+
+    def insert(self, index):
+        return
+
+    def remove(self, index):
+        return
+
+    def __getitem__(self, index):
+        return
+
+    def __iter__(self):
+        return
+
+    def __repr__(self):
+        return
