b7d8a9bbe26cc4950379cde2321af7449f00a011
The-Cooler-King <vham0927@gmail.com>
2025-06-30 19:32:27 -0700

    LLPP-0007: finished Singly Linked List class and added applicable unit tests from indexed linked list tests

--- linked-list/singly-linked-list/src/SinglyLinkedList.py
+++ linked-list/singly-linked-list/src/SinglyLinkedList.py
@@ -5,26 +5,185 @@ class SinglyLinkedList:
     def __init__(self):
         self.head = None
 
-    def append(self):
-        return
+    def append(self, value):
+        """
+        Appends a new node with the given value to the end of the list.
 
-    def prepend(self):
-        return
+        Args:
+            value: The value to store in the new node.
+        """
+        new_node = Node(value)
+
+        if not self.head:
+            self.head = new_node
+            return
+
+        current_node = self.head
+        while current_node.next:  # stop on the tail node
+            current_node = current_node.next
+
+        current_node.next = new_node
+
+    def prepend(self, value):
+        """
+        Adds a new node with the given value to the front of the list.
+
+        Args:
+            value: The value to store in the new node.
+        """
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
 
     def pop(self):
-        return
+        """
+        Removes and returns the value of the last node in the list.
 
-    def insert(self, index):
-        return
+        Returns:
+            The value of the removed node, or None if the list is empty.
+        """
+        if not self.head:
+            return None
+
+        if not self.head.next:
+            value = self.head.value
+            self.head = None
+            return value
+
+        current_node = self.head
+        while current_node.next.next:  # stop at the penultimate node
+            current_node = current_node.next
+
+        value = current_node.next.value
+        current_node.next = None
+        return value
+
+    def insert(self, index, value):
+        """
+        Inserts a new node with the given value at the specified index.
+
+        Args:
+            index (int): Position to insert the new node.
+            value: The value to store in the new node.
+
+        Raises:
+            IndexError: If the index is out of bounds.
+            TypeError: If the index is not an integer.
+        """
+        if not isinstance(index, int):
+            raise TypeError("Index must be an integer.")
+        if index < 0:
+            raise IndexError("Index out of bounds.")
+
+        new_node = Node(value)
+
+        if index == 0:
+            new_node.next = self.head
+            self.head = new_node
+            return
+
+        index_counter = 1
+        previous_node = self.head
+        current_node = self.head.next
+
+        while index != index_counter:
+            if not current_node:
+                raise IndexError("Index out of bounds.")
+            previous_node = current_node
+            current_node = current_node.next
+            index_counter += 1
+
+        previous_node.next = new_node
+        new_node.next = current_node
 
     def remove(self, index):
-        return
+        """
+        Removes the node at the specified index from the list.
+
+        Args:
+            index (int): The index of the node to remove.
+
+        Raises:
+            IndexError: If the index is out of bounds or the list is empty.
+            TypeError: If the index is not an integer.
+        """
+        if not isinstance(index, int):
+            raise TypeError("Index must be an integer.")
+        if index < 0:
+            raise IndexError("Index out of bounds.")
+        if not self.head:
+            raise IndexError("Index out of bounds. List is empty.")
+
+        if index == 0:
+            self.head = self.head.next
+            return
+
+        index_counter = 1
+        previous_node = self.head
+        current_node = self.head.next
+
+        while index != index_counter:
+            if not current_node:
+                raise IndexError("Index out of bounds.")
+            previous_node = current_node
+            current_node = current_node.next
+            index_counter += 1
+
+        if not current_node:
+            raise IndexError("Index out of bounds.")
+        previous_node.next = current_node.next
 
     def __getitem__(self, index):
-        return
+        """
+        Allows bracket-style indexing into the linked list.
+
+        Args:
+            index (int): The index of the node to retrieve.
+
+        Returns:
+            The value of the node at the given index.
+
+        Raises:
+            IndexError: If the index is out of bounds.
+            TypeError: If the index is not an integer.
+        """
+        if not isinstance(index, int):
+            raise TypeError("Index must be an integer.")
+        if index < 0:
+            raise IndexError("Index out of bounds.")
+
+        index_counter = 0
+        current_node = self.head
+
+        while current_node:
+            if index == index_counter:
+                return current_node.value
+            current_node = current_node.next
+            index_counter += 1
+
+        raise IndexError("Index out of bounds.")
 
     def __iter__(self):
-        return
+        """
+        Allows iteration over the linked list values.
+        """
+        current_node = self.head
+        while current_node:
+            yield current_node.value
+            current_node = current_node.next
 
     def __repr__(self):
-        return
+        """
+        Returns a string representation of the linked list.
+        """
+        if not self.head:
+            return "LinkedList([])"
+
+        current_node = self.head
+        node_values = []
+
+        while current_node:
+            node_values.append(str(current_node.value))
+            current_node = current_node.next
+
+        return "LinkedList([" + " -> ".join(node_values) + "])"
---
+++ linked-list/singly-linked-list/tests/test_SinglyLinkedList.py
@@ -0,0 +1,271 @@
+import unittest
+
+import sys
+import os
+
+# this line modifies sys.path inside of the test files and is necessary to import our code files without turning them into a package
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+from SinglyLinkedList import SinglyLinkedList
+from Node import Node
+
+
+class TestLinkedList(unittest.TestCase):
+
+    def test_list_initialization(self):
+        # Arrange
+        test_list = SinglyLinkedList()
+
+        # Assert
+        self.assertEqual(repr(test_list), "LinkedList([])")
+
+    def test_prepend_to_empty_list(self):
+        # Arrange
+        test_list = SinglyLinkedList()
+
+        # Act
+        test_list.prepend(5)
+
+        # Assert
+        self.assertEqual(repr(test_list), "LinkedList([5])")
+
+    def test_prepend_to_list(self):
+        # Arrange
+        test_list = SinglyLinkedList()
+        test_list.prepend(5)
+
+        # Act
+        test_list.prepend(6)
+
+        # Assert
+        self.assertEqual(repr(test_list), "LinkedList([6 -> 5])")
+
+    def test_pop_node_from_empty_list(self):
+        # Arrange
+        test_list = create_list_from_values([])
+
+        # Act
+        pop_value = test_list.pop()
+
+        # Assert
+        self.assertIsNone(pop_value)
+        self.assertEqual(repr(test_list), "LinkedList([])")
+
+    def test_pop_node_from_single_node_list(self):
+        # Arrange
+        test_list = create_list_from_values([1])
+
+        # Act
+        pop_value = test_list.pop()
+
+        # Assert
+        self.assertEqual(pop_value, 1)
+        self.assertEqual(repr(test_list), "LinkedList([])")
+
+    def test_pop_node_from_two_node_list(self):
+        # Arrange
+        test_list = create_list_from_values([1, 2])
+
+        # Act
+        pop_value = test_list.pop()
+
+        # Assert
+        self.assertEqual(pop_value, 1)
+        self.assertEqual(repr(test_list), "LinkedList([2])")
+
+    def test_pop_node_from_multiple_node_list(self):
+        # Arrange
+        test_list = create_list_from_values([1, 2, 3, 4])
+
+        # Act
+        pop_value = test_list.pop()
+
+        # Assert
+        self.assertEqual(pop_value, 1)
+        self.assertEqual(repr(test_list), "LinkedList([4 -> 3 -> 2])")
+
+    def test_iterating_over_list(self):
+        # Arrange
+        test_list = create_list_from_values(["S", "S", "E", "C", "C", "U", "S"])
+
+        # Act
+        result = ""
+        for node_value in test_list:
+            result += str(node_value)
+
+        # Assert
+        self.assertEqual(result, "SUCCESS")
+
+    def test_iterating_over_empty_list(self):
+        # Arrange
+        test_list = create_list_from_values([])
+
+        # Act
+        result = ""
+        for node_value in test_list:
+            result += str(node_value)
+
+        # Assert
+        self.assertEqual(result, "")
+
+    def test_repr_list(self):
+        # Arrange
+        test_list = create_list_from_values([1, 2, 3, 4])
+
+        # Assert
+        self.assertEqual(repr(test_list), "LinkedList([4 -> 3 -> 2 -> 1])")
+
+    def test_repr_empty_list(self):
+        # Arrange
+        test_list = create_list_from_values([])
+
+        # Assert
+        self.assertEqual(repr(test_list), "LinkedList([])")
+
+    def test_getitem_non_int_index(self):
+        # Arrange
+        test_list = create_list_from_values(["D", "C", "B", "A"])
+
+        # Act & Assert
+        with self.assertRaises(TypeError) as context:
+            test_list["1"]
+
+        self.assertEqual(str(context.exception), "Index must be an integer.")
+
+    def test_getitem_out_of_bounds_index(self):
+        # Arrange
+        test_list = create_list_from_values(["D", "C", "B", "A"])
+
+        # Act & Assert
+        with self.assertRaises(IndexError) as context:
+            test_list[-1]
+
+        self.assertEqual(str(context.exception), "Index out of bounds.")
+
+    def test_getitem_valid_index(self):
+        # Arrange
+        test_list = create_list_from_values(["D", "C", "B", "A"])
+
+        # Act
+        result = test_list[0]
+
+        # Assert
+        self.assertEqual(result, "A")
+
+    def test_insert_at_head(self):
+        # Arrange
+        test_list = create_list_from_values(["D", "C", "B", "A"])
+
+        # Act
+        test_list.insert(index=0, value="test")
+
+        # Assert
+        self.assertEqual(repr(test_list), "LinkedList([test -> A -> B -> C -> D])")
+
+    def test_insert_at_middle(self):
+        # Arrange
+        test_list = create_list_from_values(["D", "C", "B", "A"])
+
+        # Act
+        test_list.insert(index=1, value="test")
+
+        # Assert
+        self.assertEqual(repr(test_list), "LinkedList([A -> test -> B -> C -> D])")
+
+    def test_insert_at_tail(self):
+        # Arrange
+        test_list = create_list_from_values(["D", "C", "B", "A"])
+
+        # Act
+        test_list.insert(index=3, value="test")
+
+        # Assert
+        self.assertEqual(repr(test_list), "LinkedList([A -> B -> C -> test -> D])")
+
+    def test_insert_after_tail(self):
+        # Arrange
+        test_list = create_list_from_values(["D", "C", "B", "A"])
+
+        # Act
+        test_list.insert(index=4, value="test")
+
+        # Assert
+        self.assertEqual(repr(test_list), "LinkedList([A -> B -> C -> D -> test])")
+
+    def test_insert_empty_list(self):
+        # Arrange
+        test_list = create_list_from_values([])
+
+        # Act
+        test_list.insert(index=0, value="test")
+
+        # Assert
+        self.assertEqual(repr(test_list), "LinkedList([test])")
+
+    def test_remove_head(self):
+        # Arrange
+        test_list = create_list_from_values(["D", "C", "B", "A"])
+
+        # Act
+        test_list.remove(index=0)
+
+        # Assert
+        self.assertEqual("LinkedList([B -> C -> D])", repr(test_list))
+
+    def test_remove_middle_node(self):
+        # Arrange
+        test_list = create_list_from_values(["D", "C", "B", "A"])
+
+        # Act
+        test_list.remove(index=1)
+
+        # Assert
+        self.assertEqual(repr(test_list), "LinkedList([A -> C -> D])")
+
+    def test_remove_tail(self):
+        # Arrange
+        test_list = create_list_from_values(["D", "C", "B", "A"])
+
+        # Act
+        test_list.remove(index=3)
+
+        # Assert
+        self.assertEqual(repr(test_list), "LinkedList([A -> B -> C])")
+
+    def test_remove_from_empty_list(self):
+        # Arrange
+        test_list = create_list_from_values([])
+
+        # Act & Assert
+        with self.assertRaises(IndexError) as context:
+            test_list.remove(index=0)
+
+        self.assertEqual(str(context.exception), "Index out of bounds. List is empty.")
+
+    def test_append(self):
+        # Arrange
+        test_list = create_list_from_values(["D", "C", "B", "A"])
+
+        # Act
+        test_list.append("test")
+
+        # Assert
+        self.assertEqual(repr(test_list), "LinkedList([A -> B -> C -> D -> test])")
+
+    def test_append_empty_list(self):
+        # Arrange
+        test_list = create_list_from_values([])
+
+        # Act
+        test_list.append("test")
+
+        # Assert
+        self.assertEqual(repr(test_list), "LinkedList([test])")
+
+
+###### HELPER FUNCTIONS ######
+def create_list_from_values(values):
+    linked_list = SinglyLinkedList()
+    for value in values:
+        linked_list.prepend(value)
+    return linked_list
